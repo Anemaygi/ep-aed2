536938436689e5f747001933386399ef92a9cbf6
PedroMReno <pedrohmreno@gmail.com>
2022-04-21 22:20:43 -0300

    Add Componente

---
+++ EP-AED2/src/estruturas/Componente.java
@@ -0,0 +1,29 @@
+package estruturas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Componente {
+    private List<String> vertices;
+    private List<Componente> ligacoes;
+
+    public Componente(List<String> vertices) {
+        this.vertices = vertices;
+        this.ligacoes = new ArrayList<>();
+    }
+
+    public boolean contemVertice(String vertice) {
+        return vertices.contains(vertice);
+    }
+
+    public void addLigacao(Componente componente) {
+        if(componente == null)
+            return;
+
+        ligacoes.add(componente);
+    }
+
+    public List<Componente> getLigacoes() {
+        return this.ligacoes;
+    }
+}
