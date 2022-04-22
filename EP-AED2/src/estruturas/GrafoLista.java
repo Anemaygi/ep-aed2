3c5251833137079ff640287e2f0038fb2af28196
Anemaygi <gianemayumisan@gmail.com>
2022-04-22 14:52:54 -0300

    assinaturas de grafolista

--- EP-AED2/src/estruturas/GrafoLista.java
+++ EP-AED2/src/estruturas/GrafoLista.java
@@ -1,6 +1,46 @@
 package estruturas;
 
-import estruturas.grafo;
+import estruturas.No;
+import interfaces.IGrafo;
 import java.util.List;
 import java.util.ArrayList;
 
+public Class GrafoLista implements IGrafo{
+    // atributos
+    private List<No> vertices;
+    
+    // Métodos construtores   
+    public GrafoLista(String entrada){
+        // constructor 1
+    }
+
+    
+    public GrafoLista(List<Componente> componentes){
+        // constructor 2
+    }
+    
+    // Métodos da interface
+    
+    public IGrafo kosaraju(){
+
+    }
+
+    public void imprimeGrafo(){
+
+    }
+
+    public int getQtdVertices(){
+
+    }
+
+    public IGrafo getArestasTranspostas(){
+
+    }
+
+    // Métodos do GrafoLista
+    
+    private GrafoLista geraGrafoComponentes(){
+
+    }
+
+}
