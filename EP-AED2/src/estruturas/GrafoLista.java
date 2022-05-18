74e5cdbd9289e848e28f9affacd2568f98b69ce7
Anemaygi <gianemayumisan@gmail.com>
2022-05-18 00:18:03 -0300

    adicionando implementaçao, nao funcionando kosaraju...

--- EP-AED2/src/estruturas/GrafoLista.java
+++ EP-AED2/src/estruturas/GrafoLista.java
@@ -12,7 +12,7 @@ public class GrafoLista implements IGrafo{
     // atributos
     private List<No> vertices;
     
-    // Métodos construtores   
+    // Métodos construtores e inicializadores   
     public static IGrafo leGrafoDeEntrada(List<String> vertices) {
         // Adicionando os vértices
         List<No> verticesGrafo = new ArrayList<No>();
@@ -55,6 +55,73 @@ public class GrafoLista implements IGrafo{
         
     }
 
+    // Métodos da interface
+    public IGrafo kosaraju(){
+        GrafoLista transposto =(GrafoLista) this.getArestasTranspostas();
+        List<No> ordem = this.vertices;        
+        ordem = this.DFS(ordem); 
+        limpaVertices(); 
+        transposto.DFS(ordem);
+        Floresta floresta = new Floresta(transposto.vertices);
+        List<Componente> componentes = floresta.geraComponentes();
+        for(No vertice : vertices){
+            Componente compatual = Floresta.achaComponente(vertice.getValor(),componentes);
+            for(No vizinho : vertice.getVizinhos()){
+                if(compatual == Floresta.achaComponente(vizinho.getValor(),componentes)){
+                    continue;
+                }
+                compatual.addLigacao(Floresta.achaComponente(vizinho.getValor(),componentes));
+            }
+        }
+        GrafoLista grafoDeComponentes = new GrafoLista(componentes,0);
+        return grafoDeComponentes;
+    }
+
+
+    public void imprimeGrafo(){
+        List<No> componentesDFS = this.DFS(this.getVertices());
+        for (No componente : componentesDFS){
+            System.out.printf(componente+" ");
+        }
+        System.out.printf("\n");
+        for (No item : vertices) {
+            System.out.printf(item+": ");
+            for(No vizinho : item.getVizinhos()){
+                System.out.printf(vizinho +"; ");
+            }
+            System.out.printf("\n");
+        }
+    }
+
+    public int getQtdVertices(){
+        return vertices.size();
+    }
+
+    public IGrafo getArestasTranspostas(){
+        Map<String, No> mapaVersTrans = new HashMap<>();
+        List<No> novosVertices = new ArrayList<>();
+
+        //Gerando os nos pela primeira vez
+        for(No atual : vertices) {
+            No novoNoTransposto = new No(atual.getValor());
+
+            mapaVersTrans.put(atual.getValor(), novoNoTransposto);
+            novosVertices.add(novoNoTransposto); //Ja guardando os enderecos para gerar o GrafoLista
+        }
+
+        //Adicionando os vizinhos
+        for(No atual : vertices) {
+            No vizinhoTransposto = mapaVersTrans.get(atual.getValor());
+
+            for(No viz : atual.getVizinhos())
+                mapaVersTrans.get(viz.getValor()).addVizinho(vizinhoTransposto);
+        }
+
+        return new GrafoLista(novosVertices);
+    }
+
+    // Métodos da classe
+
     public ArrayList<No> DFS(List<No> ordemDFS){
         ArrayList<No> topologica = new ArrayList<No>();
         for (No vert : ordemDFS){           
@@ -83,48 +150,6 @@ public class GrafoLista implements IGrafo{
         }
     }
 
-    public IGrafo kosaraju(){
-        GrafoLista transposto =(GrafoLista) this.getArestasTranspostas();
-        List<No> ordem = this.vertices;        
-        ordem = this.DFS(ordem); 
-        limpaVertices(); 
-        transposto.DFS(ordem);
-        Floresta floresta = new Floresta(transposto.vertices);
-        List<Componente> componentes = floresta.geraComponentes();
-        for(No vertice : vertices){
-            Componente compatual = Floresta.achaComponente(vertice.getValor(),componentes);
-            for(No vizinho : vertice.getVizinhos()){
-                if(compatual == Floresta.achaComponente(vizinho.getValor(),componentes)){
-                    continue;
-                }
-                compatual.addLigacao(Floresta.achaComponente(vizinho.getValor(),componentes));
-            }
-        }
-        GrafoLista grafoDeComponentes = new GrafoLista(componentes,0);
-        return grafoDeComponentes;
-    }
-
-    public void imprimeGrafo(){
-                
-        List<No> componentesDFS = this.DFS(this.getVertices());
-        for (No componente : componentesDFS){
-            System.out.printf(componente+" ");
-        }
-        System.out.printf("\n");
-
-        for (No item : vertices) {
-            System.out.printf(item+": ");
-            for(No vizinho : item.getVizinhos()){
-                System.out.printf(vizinho +"; ");
-            }
-            System.out.printf("\n");
-        }
-    }
-
-    public int getQtdVertices(){
-        return vertices.size();
-    }
-
     public List<No> getVertices(){
         return this.vertices;
     }
@@ -136,27 +161,31 @@ public class GrafoLista implements IGrafo{
         return null;
     }
 
-    public IGrafo getArestasTranspostas(){
-        Map<String, No> mapaVersTrans = new HashMap<>();
-        List<No> novosVertices = new ArrayList<>();
+    // Métodos da aplicação
+   public String getVizinhosEmProfundidade(String valorVertice) {
+        final No idVertice = findVerticeComponent(valorVertice, this.vertices);
+        if(idVertice == null) return null;
+        this.limpaVertices();
+        return getVizinhosEmProfundidade(idVertice);
+    }
 
-        //Gerando os nos pela primeira vez
-        for(No atual : vertices) {
-            No novoNoTransposto = new No(atual.getValor());
-
-            mapaVersTrans.put(atual.getValor(), novoNoTransposto);
-            novosVertices.add(novoNoTransposto); //Ja guardando os enderecos para gerar o GrafoLista
+    public static No findVerticeComponent(String valor, List<No> lista){
+        for(No item : lista){
+            if(List.of(item.getValor().split("-")).contains(valor)) return item;}
+        return null;
+    }
+    
+    
+    private String getVizinhosEmProfundidade(No atual) {
+        String saida = atual.getValor() + " ";
+        atual.setCor(Cor.PRETO);
+        for(No vizinho : atual.getVizinhos()){
+            if(vizinho.getCor() == Cor.BRANCO){
+                saida += getVizinhosEmProfundidade(vizinho) + " ";
+            }
         }
+        return saida;
 
-        //Adicionando os vizinhos
-        for(No atual : vertices) {
-            No vizinhoTransposto = mapaVersTrans.get(atual.getValor());
-
-            for(No viz : atual.getVizinhos())
-                mapaVersTrans.get(viz.getValor()).addVizinho(vizinhoTransposto);
-        }
-
-        return new GrafoLista(novosVertices);
     }
 
     @Override
